cf3c5585282678f792411f7e211880c7bf4a32a1
Alex Ellis (VMware) <alexellis2@gmail.com>
2018-07-25 10:20:24 +0100

    Update publisher to re-connect
    The publisher will now re-connect when NATS Streaming becomes
    unavailable. Tested within the gateway code on Docker Swarm.
    
    Thanks to @vosmith for initiating this work.
    
    Signed-off-by: Alex Ellis (VMware) <alexellis2@gmail.com>

--- handler/handler.go
+++ handler/handler.go
@@ -5,13 +5,15 @@ import (
 	"fmt"
 	"log"
 
+	nats "github.com/nats-io/go-nats"
 	"github.com/nats-io/go-nats-streaming"
 	"github.com/openfaas/faas/gateway/queue"
 )
 
 // NatsQueue queue for work
 type NatsQueue struct {
-	nc        stan.Conn
+	stanConn  stan.Conn
+	natsConn  *nats.Conn
 	ClientID  string
 	ClusterID string
 	NATSURL   string
@@ -27,20 +29,32 @@ func CreateNatsQueue(address string, port int, clientConfig NatsConfig) (*NatsQu
 	clientID := clientConfig.GetClientID()
 	clusterID := "faas-cluster"
 
-	nc, err := stan.Connect(clusterID, clientID, stan.NatsURL(natsURL))
 	queue1 := NatsQueue{
-		nc:        nc,
 		ClientID:  clientID,
 		ClusterID: clusterID,
 		NATSURL:   natsURL,
 		Topic:     "faas-request",
 	}
 
+	natsConn, err := nats.Connect(natsURL, nats.ReconnectHandler(queue1.reconnectClient(clientID, clusterID, natsURL)))
+	if err != nil {
+		return nil, err
+	}
+
+	stanConn, err := stan.Connect(clusterID, clientID, stan.NatsConn(natsConn))
+	if err != nil {
+		return nil, err
+	}
+
+	queue1.natsConn = natsConn
+	queue1.stanConn = stanConn
+
 	return &queue1, err
 }
 
 // Queue request for processing
 func (q *NatsQueue) Queue(req *queue.Request) error {
+
 	var err error
 
 	fmt.Printf("NatsQueue - submitting request: %s.\n", req.Function)
@@ -50,7 +64,26 @@ func (q *NatsQueue) Queue(req *queue.Request) error {
 		log.Println(err)
 	}
 
-	err = q.nc.Publish(q.Topic, out)
+	err = q.stanConn.Publish(q.Topic, out)
 
 	return err
+}
+
+func (q *NatsQueue) reconnectClient(clientID, clusterID, natsURL string) nats.ConnHandler {
+	return func(c *nats.Conn) {
+		oldConn := q.stanConn
+
+		defer oldConn.Close()
+
+		stanConn, err := stan.Connect(clusterID, clientID, stan.NatsConn(c))
+		if err != nil {
+			log.Printf("Failed to reconnect to NATS\n%v", err)
+			return
+		} else {
+			log.Printf("Reconnected to NATS\n")
+		}
+
+		q.stanConn = stanConn
+		q.natsConn = c
+	}
 }
