fc77f3b042f13e1a56783c29bb4c9aa12d44800b
Vivek Singh <vivekkmr45@yahoo.in>
2018-09-25 01:37:44 +0530

    Remove warning message for basic auth
    This commit removes warning message when basic auth is not enabled and
    basic auth secrets are not passed.
    
    Signed-off-by: Vivek Singh <vivekkmr45@yahoo.in>

--- auth.go
+++ auth.go
@@ -10,17 +10,19 @@ import (
 
 //AddBasicAuth to a request by reading secrets
 func AddBasicAuth(req *http.Request) error {
-	reader := auth.ReadBasicAuthFromDisk{}
+	if os.Getenv("basic_auth") == "true" {
+		reader := auth.ReadBasicAuthFromDisk{}
 
-	if len(os.Getenv("secret_mount_path")) > 0 {
-		reader.SecretMountPath = os.Getenv("secret_mount_path")
+		if len(os.Getenv("secret_mount_path")) > 0 {
+			reader.SecretMountPath = os.Getenv("secret_mount_path")
+		}
+
+		credentials, err := reader.Read()
+		if err != nil {
+			return fmt.Errorf("Unable to read basic auth: %s", err.Error())
+		}
+
+		req.SetBasicAuth(credentials.User, credentials.Password)
 	}
-
-	credentials, err := reader.Read()
-	if err != nil {
-		return fmt.Errorf("Unable to read basic auth: %s", err.Error())
-	}
-
-	req.SetBasicAuth(credentials.User, credentials.Password)
 	return nil
 }
