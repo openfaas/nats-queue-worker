3ea76fda265e4c88fa2f2f9821b396352145c270
Vivek Singh <vivekkmr45@yahoo.in>
2018-10-03 00:34:54 +0530

    Load basic auth credentials at start of worker
    This commit loads basic auth credentials at the start of the queue
    worker rather than loading it from disk for every request.
    
    Signed-off-by: Vivek Singh <vivekkmr45@yahoo.in>

--- auth.go
+++ auth.go
@@ -26,3 +26,18 @@ func AddBasicAuth(req *http.Request) error {
 	}
 	return nil
 }
+
+//LoadCredentials load credentials from dis
+func LoadCredentials() (*auth.BasicAuthCredentials, error) {
+	reader := auth.ReadBasicAuthFromDisk{}
+
+	if len(os.Getenv("secret_mount_path")) > 0 {
+		reader.SecretMountPath = os.Getenv("secret_mount_path")
+	}
+
+	credentials, err := reader.Read()
+	if err != nil {
+		return nil, fmt.Errorf("Unable to read basic auth: %s", err.Error())
+	}
+	return credentials, nil
+}
