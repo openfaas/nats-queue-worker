cbed5ba6fbfdfd9f6506441d6650edd1c054ed39
Bart Smykla <bsmykla@vmware.com>
2019-01-14 15:43:20 +0000

    Added reconnection logic when NATS is disconnected
    How was it tested:
    I deployed this version of nats-queue-worker and then simulated
    NATS disconnection
    
    Signed-off-by: Bart Smykla <bsmykla@vmware.com>

--- handler/nats_config.go
+++ handler/nats_config.go
@@ -2,21 +2,38 @@ package handler
 
 import (
 	"os"
+	"time"
 
 	"github.com/openfaas/nats-queue-worker/nats"
 )
 
 type NatsConfig interface {
 	GetClientID() string
+	GetMaxReconnect() int
+	GetReconnectDelay() time.Duration
 }
 
 type DefaultNatsConfig struct {
+	maxReconnect   int
+	reconnectDelay time.Duration
+}
+
+func NewDefaultNatsConfig(maxReconnect int, reconnectDelay time.Duration) DefaultNatsConfig {
+	return DefaultNatsConfig{maxReconnect, reconnectDelay}
 }
 
 // GetClientID returns the ClientID assigned to this producer/consumer.
 func (DefaultNatsConfig) GetClientID() string {
 	val, _ := os.Hostname()
 	return getClientID(val)
+}
+
+func (c DefaultNatsConfig) GetMaxReconnect() int {
+	return c.maxReconnect
+}
+
+func (c DefaultNatsConfig) GetReconnectDelay() time.Duration {
+	return c.reconnectDelay
 }
 
 func getClientID(hostname string) string {
